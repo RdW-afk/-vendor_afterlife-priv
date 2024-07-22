03e51394ff3433150e070fe41b24992f7a97ef92
romeo <rbesar210311@gmail.com>
2024-07-22 02:21:25 +0200

    Add Warning While build
    Signed-off-by: romeo <rbesar210311@gmail.com>

--- keys/keys.mk
+++ keys/keys.mk
@@ -1,1 +1,2 @@
-PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/extra/keys/releasekey
+$(warning "Your build signed with Romeo keys.")
+PRODUCT_DEFAULT_DEV_CERTIFICATE := vendor/afterlife-priv/keys/releasekey
